55bfe45cd4ed8bc28bb09a9c6d45f2b16b40687d
Akif Abdullah <akifabdullah199gmail.com>
2024-11-28 21:23:56 +0530

    3rd Project

--- Project2-Vowel-Counter-Application/script.js
+++ Project2-Vowel-Counter-Application/script.js
@@ -1,27 +1,17 @@
 function checkVowels(){
-
-    let text = document.getElementById("inputText").value;
-    let vowelCount = 0;
-   
-     text = text.toLowerCase();
-   
-     for(let i = 0; i < text.length; i++){
-   
-       let char = text.charAt(i);
-       if (isVowel(char)){
-           vowelCount++;
-       }
-     }
-   
-       
-     const result = document.getElementById('result');
-     result.textContent = "Total Vowels: " + vowelCount;
-   
-   
-   }
-   
-   
-   function isVowel(char){
-       const vowels = ["a","e","i","o","u"];
-       return vowels.includes(char);
-   }
+  let text=document.getElementById("inputText").value;
+  let vowelcount=0;
+  text=text.toLowerCase();
+  for(let i=0;i<text.length;i++){
+    let char=text.charAt(i);
+    if(vowels(char)){
+      vowelcount++;
+    }
+  }
+  const result=document.getElementById("result");
+  result.textContent="Total vowels:"+vowelcount;
+}
+function vowels(char){
+  const vowels=["a","e","i","o","u"];
+  return vowels.includes(char);
+}
